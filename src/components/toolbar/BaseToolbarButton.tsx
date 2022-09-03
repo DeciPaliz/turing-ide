a1486a2b256016095d35b3bc2a0a5602376e218c
DeciPaliz <unat7919@gmail.com>
2022-09-03 15:07:33 +0500

    more styles

--- src/components/toolbar/BaseToolbarButton.tsx
+++ src/components/toolbar/BaseToolbarButton.tsx
@@ -26,7 +26,7 @@ export default class BaseToolbarButton extends React.Component<BaseToolbarButton
 
     render() {
         return (
-            <div className={this.className}>
+            <div className="BaseToolbarButton" onClick={this.click}>
                 <img src={this.icon} width="50px" height="50px"></img>
             </div>
         );
